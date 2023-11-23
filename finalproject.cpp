4010b6915f38ebcb85949ad325962ff3e2f32dfa
MarcusLieu <marcus.lieu4@gmail.com>
2023-11-23 16:29:27 -0700

    added breaks

--- finalproject.cpp
+++ finalproject.cpp
@@ -22,11 +22,18 @@ int main(void) {
     while (choice != 0) {
         switch(menu()) {
             case 1:
+                break;
             case 2:
+                break;
             case 3:
+                break;
             case 4:
+                break;
             case 5:
+                break;
             case 6:
+                choice = 0;
+                break;
         }
     }
 }
